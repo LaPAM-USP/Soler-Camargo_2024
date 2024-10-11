74b2703691a30ee4e2018acc8227fe5922c9e6b0
Naila soler <84601659+nailasoler@users.noreply.github.com>
2024-10-11 17:17:40 -0300

    Update 7_lofreq.sh

--- 7_lofreq.sh
+++ 7_lofreq.sh
@@ -2,17 +2,17 @@
 
 # Indelqual
 for bamfile in *-sorted-named-dupl.bam; do 
-    lofreq indelqual --dindel -f /home/geninfo/ncamargo/MTB_IAL_2022/mapping/NC0009623.fasta -o "${bamfile%.}_lofreq.bam" "$bamfile"
+    lofreq indelqual --dindel -f NC0009623.fasta -o "${bamfile%.}_lofreq.bam" "$bamfile"
 done
 
 # Aln qual
 for bamfile in *_lofreq.bam; do 
     output_aln_qual="${bamfile%.*}_aln_qual.bam"
-    lofreq alnqual -b -r "$bamfile" /home/geninfo/ncamargo/MTB_IAL_2022/mapping/NC0009623.fasta > "$output_aln_qual"
+    lofreq alnqual -b -r "$bamfile" NC0009623.fasta > "$output_aln_qual"
 done
 
 # Variant calling
 for aln_qual_file in *_aln_qual.bam; do 
     output_vcf="${aln_qual_file%.*}_LofreqCall.vcf"
-    lofreq call -f /home/geninfo/ncamargo/MTB_IAL_2022/mapping/NC0009623.fasta -o "$output_vcf" --call-indels "$aln_qual_file"
+    lofreq call -f NC0009623.fasta -o "$output_vcf" --call-indels "$aln_qual_file"
 done
